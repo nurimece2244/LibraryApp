6d8da42e89a0b7454f20812e823c9df3674cacd8
nur-imece <nur.imece@kafein.com.tr>
2023-09-17 18:51:50 +0300

    fix

---
+++ LibraryApp/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryApp.Controllers;
+
+// Controllers/AccountController.cs
+
+public class AccountController
+{
+    private readonly SignInManager<IdentityUser> _signInManager;
+
+    public AccountController(SignInManager<IdentityUser> signInManager)
+    {
+        _signInManager = signInManager;
+    }
+
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Login( LoginViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+
+        var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password, false, false);
+        
+        if (result.Succeeded)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        ModelState.AddModelError(string.Empty, "Geçersiz giriş denemesi.");
+        return View(model);
+    }
+}
---
+++ LibraryApp/Models/LoginViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace LibraryApp.Models;
+
+public class LoginViewModel
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
